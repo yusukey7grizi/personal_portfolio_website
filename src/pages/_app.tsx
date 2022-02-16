0c9260d7b3f1d5409017eb4646c8f37eb8816020
Yusuke Yamane <yusukey7grizi@gmail.com>
2022-02-16 17:51:16 +0900

    done with setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import 'components/globals.css';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
