d38780336323a0259b995f80640f3d7c1fa9c102
Yusuke Yamane <yusukey7grizi@gmail.com>
2022-03-24 00:32:14 +0900

    renew the home page template

--- src/components/atoms/underline/index.tsx
+++ src/components/atoms/underline/index.tsx
@@ -4,7 +4,7 @@ import { Colors } from 'components/constants';
 export const UnderLine = styled('div')(() => ({
   width: '5rem',
   height: '0.4rem',
-  backgroundColor: Colors.blue,
+  backgroundColor: Colors.pink,
   borderRadius: '0.2rem',
   top: '0.6rem',
   margin: 'auto',
