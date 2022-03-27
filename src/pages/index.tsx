f9b08515671187d8517d3669fc0c7b1d2df080bf
Yusuke Yamane <yusukey7grizi@gmail.com>
2022-03-28 00:43:06 +0900

    finish initializing firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,25 @@ import { EducationPage } from 'components/templates/educationPage';
 import { ExperiencePage } from 'components/templates/experiencePage';
 import { HomePage } from 'components/templates/homePage';
 import { ProjectPage } from 'components/templates/projectPage';
+import { collection, getDocs } from 'firebase/firestore/lite';
+import { db } from 'firebaseConfig';
 import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
 const Page: NextPage = () => {
+  const [dataSet, setDataSet] = useState<any>(null);
+
+  const runFunction = async () => {
+    const citiesCol = await collection(db, 'user');
+    const citySnapshot = await getDocs(citiesCol);
+    const cityList = citySnapshot.docs.map((doc) => doc.data());
+    setDataSet(cityList);
+  };
+
+  useEffect(() => {
+    runFunction();
+  }, []);
+
   return (
     <>
       <HomePage />
